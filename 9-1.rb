9e32cce372e8887bf3fea4d5450e5959afa5296e
keikishi <59759487+keikishi@users.noreply.github.com>
2021-08-21 14:55:52 +0900

    Update 9-1.rb

--- 9-1.rb
+++ 9-1.rb
@@ -1,1 +1,21 @@
+class Coordinate
+    attr_reader :pointX, :pointY
 
+    def initialize(pointX, pointY)
+        @pointX = pointX
+        @pointY = pointY
+    end
+
+    def to_s
+        "(#{pointX}, #{pointY})"
+    end
+end
+
+puts "座標pを入力せよ。"
+print "x座標："
+x = gets.to_i
+print "y座標："
+y = gets.to_i
+
+zahyo_p = Coordinate.new(x, y)
+puts "p = #{zahyo_p.to_s}"
