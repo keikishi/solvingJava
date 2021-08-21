bea7d694d304a013d94ca14182cbb77f4e549b79
keikishi <59759487+keikishi@users.noreply.github.com>
2021-08-21 14:55:24 +0900

    Update 9-2.rb

--- 9-2.rb
+++ 9-2.rb
@@ -1,1 +1,20 @@
+class Coordinate
+    attr_accessor :pointX, :pointY
 
+    def initialize(pointX, pointY)
+       @pointX = pointX
+       @pointY = pointY        
+    end
+
+    private
+    def self.original(pointX, pointY)
+
+    end
+end
+
+puts Coordinate.original
+
+zahyo = Coordinate.new(5, 5)
+puts "(#{zahyo.pointX}, #{zahyo.pointY})"
+
+puts zahyo.pointX
