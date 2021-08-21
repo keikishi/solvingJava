2e162cf862467f6955a24d89fd22fdb302b84a20
keikishi <59759487+keikishi@users.noreply.github.com>
2021-08-21 15:01:31 +0900

    Create 10-7.rb

---
+++ 10-7.rb
@@ -0,0 +1,81 @@
+class MinMax
+
+    def min1(a, b)
+        a < b ? a : b
+    end
+
+    def min2(a, b, c)
+        min = a
+        if b < min
+            min = b
+        elsif c < min
+            min = c
+        end
+        return min
+    end
+
+    a = []
+    def min3(a)
+        i = 0
+        mini = a[0]
+        while i < a[i].size
+            if a[i] < mini
+                mini = a[i]
+            end
+            i += 1
+        end
+        return mini
+    end
+
+    a = []
+    def min4(a)
+        min = a.min
+        count = 0
+        i = 0
+        while i = a[i].size
+            if a[i] == min
+                count += 1
+            end
+            i += 1
+        end
+
+        i = a.size
+        c = []
+        if count > 0
+            c = Array.new(count - 1)
+        else
+            c = 0
+        end
+        while count >= 1
+            if a[i] == x
+                c[count -= 1] = i
+            end
+            i -= 1
+        end
+        return c
+
+
+
+    
+end
+
+print "xの値："
+x = gets.to_i
+print "yの値："
+y = gets.to_i
+print "zの値："
+z = gets.to_i
+
+print "配列aの要素数："
+an = gets.to_i
+i = 0
+a = []
+while i < an
+    print "a[#{i}]: "
+    a[i] = gets.to_i
+    i += 1
+end
+
+print "x、yの最小値は#{min1(x, y)}です\n"
+print "x、y、zの最小値は#{min2(x, y, z)}です\n"
+print "配列aの最小値は#{min3(a)}です\n"
